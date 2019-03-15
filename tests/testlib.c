539bdaf8fccd31153a0081cb4c07d3de20ba84a0
Marian Domanski <mmdski@gmail.com>
2019-03-14 19:46:04 -0500

    corrected floating error in test_is_close

--- tests/testlib.c
+++ tests/testlib.c
@@ -1,7 +1,7 @@
 #include "testlib.h"
 
 bool test_is_close(double a, double b, double abs_tol, double rel_tol) {
-    if (fabs(a - b) <= (abs_tol + rel_tol * abs(b))) {
+    if (fabs(a - b) <= (abs_tol + rel_tol * fabs(b))) {
         return true;
     } else {
         return false;
