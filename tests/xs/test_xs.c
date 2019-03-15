203ae1a7320a318d88b67580e6d397ec804cece5
Marian Domanski <mmdski@gmail.com>
2019-03-14 20:02:31 -0500

    removed top level test, added xs test

--- tests/xs/test_xs.c
+++ tests/xs/test_xs.c
@@ -6,3 +6,9 @@ void add_xs_tests(void) {
     add_coord_tests();
     add_subsection_tests();
 }
+
+int main(int argc, char *argv[]) {
+    g_test_init(&argc, &argv, NULL);
+    add_xs_tests();
+    return g_test_run();
+}
