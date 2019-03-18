ccd3d29aae3f7b9ae365ce4d665d94da102f7747
Marian Domanski <mmdski@gmail.com>
2019-03-18 10:20:17 -0500

    add init exception handling to coarray test

--- tests/xs/test_coarray.c
+++ tests/xs/test_coarray.c
@@ -1,13 +1,18 @@
 #include "cii/mem.h"
 #include "coarray.h"
 #include "testlib.h"
+#include <stdbool.h>
 #include <stdio.h>
 
 #define ABS_TOL 1e-10
 #define REL_TOL 0
 
+/* no exception placeholder */
+const Except_T no_Error;
+
 typedef struct {
     CoArray ca;
+    const Except_T *caught_exception;
 } CoArrayFixture;
 
 typedef struct CoArrayTestData {
@@ -15,22 +20,30 @@ typedef struct CoArrayTestData {
     double *y1;
     double *z1;
 
-    int n2;
-    double *y2;
-    double *z2;
+    double ylo;
+    double yhi;
 
     CoArray expected;
+
+    const Except_T *exception;
 } CoArrayTestData;
 
-CoArrayTestData *new_coarray_test_data(int n1, double *y1, double *z1, int n2,
-                                       double *y2, double *z2, CoArray expected);
+CoArrayTestData *new_coarray_test_data(int n1, double *y1, double *z1,
+                                       double ylo, double yhi,
+                                       CoArray expected,
+                                       const Except_T *exception);
 void coarray_test_free(CoArrayTestData *cat);
 void check_coarray_values(CoArray ca, double *y, double *z);
 
 /* coarray_new test functions */
 
 void init_coarray_new(CoArrayFixture *caf, CoArrayTestData test_data) {
-    caf->ca = coarray_new(test_data.n1, test_data.y1, test_data.z1);
+    caf->caught_exception = &no_Error;
+    TRY
+        caf->ca = coarray_new(test_data.n1, test_data.y1, test_data.z1);
+    EXCEPT(coarray_y_order_Error)
+        caf->caught_exception = &coarray_y_order_Error;
+    END_TRY;
 }
 
 void coarray_new_setup(CoArrayFixture *caf, gconstpointer test_data) {
@@ -44,13 +57,18 @@ void coarray_new_teardown(CoArrayFixture *caf, gconstpointer test_data) {
 
 void coarray_test_new(CoArrayFixture *caf, gconstpointer test_data) {
     CoArrayTestData data = *(const CoArrayTestData *)test_data;
-    check_coarray_values(caf->ca, data.y1, data.z1);
+    const Except_T *caught_exception = caf->caught_exception;
+    g_assert_true(caught_exception == data.exception);
+    if (data.exception == &no_Error)
+        check_coarray_values(caf->ca, data.y1, data.z1);
 }
 
 /* test utility functions */
 
-CoArrayTestData *new_coarray_test_data(int n1, double *y1, double *z1, int n2,
-                                       double *y2, double *z2, CoArray expected) {
+CoArrayTestData *new_coarray_test_data(int n1, double *y1, double *z1,
+                                       double ylo, double yhi,
+                                       CoArray expected,
+                                       const Except_T *exception) {
     CoArrayTestData *cat;
     NEW(cat);
 
@@ -63,16 +81,12 @@ CoArrayTestData *new_coarray_test_data(int n1, double *y1, double *z1, int n2,
         cat->z1 = NULL;
     }
 
-    cat->n2 = n2;
-    if (n2 > 0) {
-        cat->y2 = Mem_calloc(n2, sizeof(double), __FILE__, __LINE__);
-        cat->z2 = Mem_calloc(n2, sizeof(double), __FILE__, __LINE__);
-    } else {
-        cat->y2 = NULL;
-        cat->z2 = NULL;
-    }
+    cat->ylo = ylo;
+    cat->yhi = yhi;
+
 
     cat->expected = expected;
+    cat->exception = exception;
 
     return cat;
 }
@@ -82,11 +96,6 @@ void coarray_test_free(CoArrayTestData *cat) {
     if (cat->n1 > 0) {
         Mem_free(cat->y1, __FILE__, __LINE__);
         Mem_free(cat->z1, __FILE__, __LINE__);
-    }
-
-    if (cat->n2 > 0) {
-        Mem_free(cat->y2, __FILE__, __LINE__);
-        Mem_free(cat->z2, __FILE__, __LINE__);
     }
 
     if (cat->expected)
@@ -115,14 +124,12 @@ void check_coarray_values(CoArray ca, double *y, double *z) {
 
 void add_coarray_tests(void) {
 
-    int n1 = 2;
-    double y1[] = {0, 1};
-    double z1[] = {0.5, 1.5};
-    CoArrayTestData *test_data =  new_coarray_test_data(n1, y1, z1, 0, NULL,
-                                                        NULL, NULL);
-    g_test_add("/panthera/xs/coarray/new", CoArrayFixture,
-               test_data, coarray_new_setup, coarray_test_new,
+    int n1 = 4;
+    double y1[] = {0, 0.25, 0.5, 1};
+    double z1[] = {1, 1.5, 1.25, 1};
+    CoArrayTestData *test_data_1 =  new_coarray_test_data(n1, y1, z1, NAN, NAN,
+                                                          NULL, &no_Error);
+    g_test_add("/panthera/xs/coarray/new/success", CoArrayFixture,
+               test_data_1, coarray_new_setup, coarray_test_new,
                coarray_new_teardown);
-
-
 }
