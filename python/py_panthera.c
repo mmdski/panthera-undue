821fc08ae6d43a543df8eed14b0ed02ceffb87fb
Marian Domanski <mmdski@gmail.com>
2019-05-06 17:27:22 -0500

    x to y, y to z in PyCrossSection

--- python/py_panthera.c
+++ python/py_panthera.c
@@ -4,18 +4,20 @@
 #define NPY_NO_DEPRECATED_API NPY_API_VERSION
 #include <numpy/arrayobject.h>
 
+#include <panthera/coarray.h>
 #include <panthera/crosssection.h>
 
 typedef struct {
     PyObject_HEAD    /* */
-        PyObject *x; /* x values */
-    PyObject *    y; /* y values */
+        PyObject *y; /* y values */
+    PyObject *    z; /* z values */
 } PyCrossSection;
 
 static void
 PyCrossSection_dealloc (PyCrossSection *self)
 {
-    Py_XDECREF (self->x);
+    Py_XDECREF (self->y);
+    Py_XDECREF (self->z);
     Py_TYPE (self)->tp_free ((PyObject *) self);
 }
 
@@ -24,34 +26,57 @@ PyCrossSection_new (PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
     PyCrossSection *self;
     self    = (PyCrossSection *) type->tp_alloc (type, 0);
-    self->x = NULL;
     self->y = NULL;
+    self->z = NULL;
     return (PyObject *) self;
 }
 
 static int
 PyCrossSection_init (PyCrossSection *self, PyObject *args, PyObject *kwds)
 {
-    PyObject *x = NULL;
+    int     size;
+    double *y_data;
+    double *z_data;
+
     PyObject *y = NULL;
+    PyObject *z = NULL;
 
-    if (!PyArg_ParseTuple (args, "OO", &x, &y))
+    if (!PyArg_ParseTuple (args, "OO", &y, &z))
         return -1;
 
-    self->x = PyArray_FROM_OTF (x, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
-    if (self->x == NULL)
-        return -1;
-
-    self->y = PyArray_FROM_OTF (y, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    self->y = PyArray_FROM_OTF (y, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS);
     if (self->y == NULL)
         return -1;
+
+    self->z = PyArray_FROM_OTF (z, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS);
+    if (self->z == NULL)
+        return -1;
+
+    /* y and z must be 1D */
+    if (PyArray_NDIM ((PyArrayObject *) self->y) != 1 ||
+        PyArray_NDIM ((PyArrayObject *) self->z) != 1) {
+        PyErr_SetString (PyExc_ValueError, "y and z must be 1D arrays");
+        return -1;
+    }
+
+    /* y and z must be the same size */
+    if (PyArray_SIZE ((PyArrayObject *) self->y) !=
+        PyArray_SIZE ((PyArrayObject *) self->z)) {
+        PyErr_SetString (PyExc_ValueError,
+                         "the size of y and z must be equal");
+        return -1;
+    }
+
+    size   = PyArray_SIZE ((PyArrayObject *) self->y);
+    y_data = (double *) PyArray_DATA ((PyArrayObject *) self->y);
+    z_data = (double *) PyArray_DATA ((PyArrayObject *) self->z);
 
     return 0;
 }
 
 static PyMemberDef PyCrossSection_members[] = {
-    { "x", T_OBJECT_EX, offsetof (PyCrossSection, x), 0, "x" },
     { "y", T_OBJECT_EX, offsetof (PyCrossSection, y), 0, "y" },
+    { "z", T_OBJECT_EX, offsetof (PyCrossSection, z), 0, "z" },
     { NULL }
 };
 
--- python/test_pantherapy.py
+++ python/test_pantherapy.py
@@ -16,4 +16,26 @@ class TestCrossSection(unittest.TestCase):
         xs = CrossSection(x, y)
         self.assertIsNotNone(xs)
 
-        self.assertRaises(ValueError, CrossSection, *('a', 'b'))
+        # non-float check
+        message = 'could not convert string to float: \'a\''
+        self.assertRaisesRegex(ValueError, message, CrossSection, *('a', y))
+
+        # 1D check
+        x_2D = np.array([x])
+        y_2D = np.array([y])
+        one_d_message = 'y and z must be 1D arrays'
+        self.assertRaisesRegex(ValueError, one_d_message,
+                               CrossSection, *(x_2D, y))
+        self.assertRaisesRegex(ValueError, one_d_message,
+                               CrossSection, *(x, y_2D))
+        self.assertRaisesRegex(ValueError, one_d_message, CrossSection,
+                               *(x_2D, y_2D))
+
+        # equal size check
+        x_big = np.array([1, 1, 2, 3, 3, 3])
+        y_big = np.array([1, 0, 0, 0, 1, 2])
+        big_message = 'the size of y and z must be equal'
+        self.assertRaisesRegex(ValueError, big_message,
+                               CrossSection, *(x_big, y))
+        self.assertRaisesRegex(ValueError, big_message,
+                               CrossSection, *(x, y_big))
