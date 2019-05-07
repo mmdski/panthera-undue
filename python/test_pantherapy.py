6c5a385423cf1346d6efe2492da7a4ea7f0e49c0
Marian Domanski <mmdski@gmail.com>
2019-05-06 20:08:09 -0500

    correct y-z in test

--- python/test_pantherapy.py
+++ python/test_pantherapy.py
@@ -11,31 +11,32 @@ class TestCrossSection(unittest.TestCase):
         """Test initialization of a CrossSection"""
 
         # test successful init
-        x = np.array([1, 1, 2, 3, 3])
         y = np.array([1, 0, 0, 0, 1])
-        xs = CrossSection(x, y)
+        z = np.array([1, 1, 2, 3, 3])
+        xs = CrossSection(y, z)
         self.assertIsNotNone(xs)
 
         # non-float check
         message = 'could not convert string to float: \'a\''
-        self.assertRaisesRegex(ValueError, message, CrossSection, *('a', y))
+        self.assertRaisesRegex(ValueError, message, CrossSection, *('a', z))
 
         # 1D check
-        x_2D = np.array([x])
         y_2D = np.array([y])
+        z_2D = np.array([z])
         one_d_message = 'y and z must be 1D arrays'
         self.assertRaisesRegex(ValueError, one_d_message,
-                               CrossSection, *(x_2D, y))
+                               CrossSection, *(y_2D, y))
         self.assertRaisesRegex(ValueError, one_d_message,
-                               CrossSection, *(x, y_2D))
+                               CrossSection, *(y, z_2D))
         self.assertRaisesRegex(ValueError, one_d_message, CrossSection,
-                               *(x_2D, y_2D))
+                               *(y_2D, z_2D))
 
         # equal size check
-        x_big = np.array([1, 1, 2, 3, 3, 3])
         y_big = np.array([1, 0, 0, 0, 1, 2])
+        z_big = np.array([1, 1, 2, 3, 3, 3])
         big_message = 'the size of y and z must be equal'
         self.assertRaisesRegex(ValueError, big_message,
-                               CrossSection, *(x_big, y))
+                               CrossSection, *(y_big, z))
         self.assertRaisesRegex(ValueError, big_message,
-                               CrossSection, *(x, y_big))
+                               CrossSection, *(y, z_big))
+#
