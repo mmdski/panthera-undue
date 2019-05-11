51a41ca4efec76ec526d773de3808f5e88170d08
Marian Domanski <mmdski@gmail.com>
2019-05-11 11:09:26 -0500

    add plot module

---
+++ python/pantherapy/plot.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+
+
+class CrossSectionPlot:
+    """Pantherapy cross section plots
+
+    Parameters
+    ----------
+    cross_section : pantherapy.CrossSection
+
+    """
+
+    def __init__(self, cross_section):
+
+        self._xs = cross_section
+
+    def plot(self, h=None, ax=None):
+        """Plot cross section geometry
+
+        Parameters
+        ----------
+        h : float, optional
+            Water surface elevation relative to cross section coordinate 
+            elevations, optional (default is None). If not None, wetted 
+            properties will be plotted if h is greater than the lowest 
+            y-coordinate in the cross section.
+        ax : matplotlib.axes.Axes, optional
+            Axes to plot on, optional (the default is None, which creates a 
+            new Axes instance).
+
+        Returns
+        -------
+        matplotlib.axes.Axes
+
+        """
+
+        if ax is None:
+
+            ax = plt.axes()
+
+        y, z = self._xs.coordinates()
+        ax.plot(z, y, 'k', marker='.', label='Coordinates')
+
+        if h is not None and h > y.min():
+
+            y_wp, z_wp = self._xs.wp_array(h)
+            y_tw, z_tw = self._xs.tw_array(h)
+
+            xs_area_zy = [*zip(z_wp, y_wp)]
+
+            if h > y_wp[0]:
+                xs_area_zy.insert(0, (z_wp[0], h))
+            if h > y_wp[-1]:
+                xs_area_zy.append((z_wp[-1], h))
+
+            ax.plot(z_tw, y_tw, 'b', linewidth=2.5, label='Top width')
+            ax.plot(z_wp, y_wp, 'g', linewidth=5, label='Wetted perimeter')
+
+            if len(xs_area_zy) > 2:
+                poly = Polygon(xs_area_zy, facecolor='b',
+                               alpha=0.25, label='Wetted area')
+                ax.add_patch(poly)
+
+        ax.set_xlabel('z, in meters')
+        ax.set_ylabel('y, in meters')
+
+        ax.legend()
+
+        return ax
