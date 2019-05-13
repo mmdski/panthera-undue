bd2a60c5a3cfbc316ccca76afcd16b52d1019b26
Marian Domanski <mmdski@gmail.com>
2019-05-13 18:15:22 -0500

    refactor as python package

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, Extension
+
+import os
+import glob
+
+import numpy as np
+
+panthera_inc = os.path.abspath('pantherapy/panthera/include/')
+panthera_src = glob.glob('pantherapy/panthera/src/**/*.c', recursive=True)
+
+pantherapy_src = ['pantherapy/py_panthera.c']
+pantherapy_src.extend(panthera_src)
+
+xsmodule = Extension('pantherapy.panthera',
+                     sources=pantherapy_src,
+                     include_dirs=[
+                         panthera_inc,
+                         np.get_include()])
+
+setup(name="pantherapy",
+      version="0.0.0",
+      url="https://github.com/mmdski/panthera-undue",
+      author="Marian Domanski",
+      author_email="mmdski@gmail.com",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python 3',
+          'OSI Approved :: GNU General Public License v3 (GPLv3)'
+      ],
+      description="1-D hydraulics computation library",
+      license="GPLv3",
+      packages=["pantherapy"],
+      ext_modules=[xsmodule],
+      install_requires=["numpy", "matplotlib"]
+      )
