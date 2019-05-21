e514e6d5e49af882ebe1ebb4fcf72c3097f97628
Marian Domanski <mmdski@gmail.com>
2019-05-20 21:07:54 -0500

    add reach init tests

--- tests/test_reach.py
+++ tests/test_reach.py
@@ -17,9 +17,70 @@ class TestReach(unittest.TestCase):
         xs = CrossSection(xs_y, xs_z, roughness)
 
         slope = 0.001
-        x = np.linspace(0, 1e3, num=50)
+        x = np.linspace(0, 1e3)
         y = slope * x
         xs_numbers = np.zeros_like(x, dtype='int32')
         xs_table = {0: xs}
         reach = Reach(x, y, xs_numbers, xs_table)
         self.assertIsNotNone(reach)
+
+        # test init with non-dict_like object as table
+        self.assertRaisesRegex(
+            AttributeError, "'NoneType' object has no attribute 'items'",
+            Reach, *(x, y, xs_numbers, None))
+
+        # test failure with 2D arrays
+        x_2D = np.array([x])
+        y_2D = np.array([y])
+        xs_numbers_2D = np.array([xs_numbers])
+        self.assertRaisesRegex(
+            ValueError, "'x' must be 1D", Reach, *
+            (x_2D, y, xs_numbers, xs_table)
+        )
+        self.assertRaisesRegex(
+            ValueError, "'y' must be 1D", Reach, *
+            (x, y_2D, xs_numbers, xs_table)
+        )
+        self.assertRaisesRegex(
+            ValueError, "'xs_number' must be 1D", Reach, *
+            (x, y, xs_numbers_2D, xs_table)
+        )
+
+        x_small = x[0:-1]
+        y_small = y[0:-1]
+        xs_numbers_small = xs_numbers[0:-1]
+
+        # test failure with irregularly size array
+        self.assertRaisesRegex(
+            ValueError, "The size of x, y, and xs_number must be equal",
+            Reach, *(x_small, y, xs_numbers, xs_table)
+        )
+        self.assertRaisesRegex(
+            ValueError, "The size of x, y, and xs_number must be equal",
+            Reach, *(x, y_small, xs_numbers, xs_table)
+        )
+        self.assertRaisesRegex(
+            ValueError, "The size of x, y, and xs_number must be equal",
+            Reach, *(x, y, xs_numbers_small, xs_table)
+        )
+
+        # test failure with non-cross section values in xs_table
+        xs_table_no_xs = {0: 1}
+        self.assertRaisesRegex(
+            TypeError, "xs_table values must be cross section type",
+            Reach, *(x, y, xs_numbers, xs_table_no_xs)
+        )
+
+        # test failure with incorrect keys
+        xs_numbers_ones = np.ones_like(xs_numbers)
+        self.assertRaisesRegex(
+            ValueError, "Values in 'xs_number' must be keys in 'xs_table'",
+            Reach, *(x, y, xs_numbers_ones, xs_table)
+        )
+
+        # test failure with x in descending order
+        x_descending = np.linspace(1, 0)
+        self.assertRaisesRegex(
+            ValueError, "Values in 'x' must be in ascending or equal order",
+            Reach, *(x_descending, y, xs_numbers, xs_table)
+        )
