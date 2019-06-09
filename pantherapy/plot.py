2586ef70d461ac17afcb695da157ffe399bb5fec
Marian Domanski <mmdski@gmail.com>
2019-06-09 00:41:24 -0500

    add standard step results plot class

--- pantherapy/plot.py
+++ pantherapy/plot.py
@@ -1,10 +1,12 @@
+"""Pantherapy plots"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 
 
 class CrossSectionPlot:
-    """Pantherapy cross section plots
+    """Cross section plots
 
     Parameters
     ----------
@@ -27,7 +29,7 @@ class CrossSectionPlot:
             elevations, optional (default is None). If not None, wetted
             properties will be plotted if h is greater than the lowest
             y-coordinate in the cross section.
-        ax : matplotlib.axes.Axes, optional
+        ax : :class:`matplotlib.axes.Axes`, optional
             Plot axes, optional (the default is None, which creates a
             new Axes instance).
 
@@ -66,6 +68,58 @@ class CrossSectionPlot:
                 ax.add_patch(poly)
 
         ax.set_xlabel('z, in meters')
+        ax.set_ylabel('y, in meters')
+
+        ax.legend()
+
+        return ax
+
+
+class SStepResultsPlot:
+    """Standard step solution results plots
+
+    Parameters
+    ----------
+    results : :class:`pantherapy.panthera.StandardStepResults`
+        Results to plot
+
+    """
+
+    def __init__(self, results):
+
+        self._res = results
+
+    def plot(self, ax=None):
+        """Plot standard step solution results
+
+        Parameters
+        ----------
+        ax : :class:`matplotlib.axes.Axes`, optional
+            Plot axes, optional (the default is None, which creates a
+            new Axes instance).
+
+        Returns
+        -------
+        matplotlib.axes.Axes
+            Plot axes
+
+        """
+
+        if ax is None:
+            ax = plt.axes()
+
+        x_tw, y_tw = self._res.thalweg()
+        x_ws, y_ws = self._res.ws_elevation()
+
+        poly_pts = [*zip(x_tw, y_tw)]
+        poly_pts.extend([*zip(np.flip(x_ws), np.flip(y_ws))])
+        poly = Polygon(poly_pts, facecolor='b', alpha=0.25)
+        ax.add_patch(poly)
+
+        ax.plot(x_ws, y_ws, 'b', linewidth=2.5, label='Water surface')
+        ax.plot(x_tw, y_tw, 'g', label='Thalweg')
+
+        ax.set_xlabel('x, in meters')
         ax.set_ylabel('y, in meters')
 
         ax.legend()
