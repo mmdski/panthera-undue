206326c152a7ff6afed314cb4e033bf38f0d2999
Marian Domanski <mmdski@gmail.com>
2019-06-24 19:54:21 -0500

    add critical depth unit test

--- pantherapy/panthera/tests/xs/test_crosssection.c
+++ pantherapy/panthera/tests/xs/test_crosssection.c
@@ -426,6 +426,39 @@ test_xs_n_roughness (void)
 }
 
 void
+test_critical_depth (void)
+{
+    int     i;
+    int     n           = 5;
+    double  z[]         = { 0, 0, 0.5, 1, 1 };
+    double  y[]         = { 1, 0, 0, 0, 1 };
+    int     n_roughness = 1;
+    double  r[]         = { 0.030 };
+    double *z_r         = NULL;
+
+    double depth;
+    double critical_flow;
+    double critical_depth;
+
+    CoArray           ca = coarray_new (n, y, z);
+    CrossSection      xs = xs_new (ca, n_roughness, r, z_r);
+    CrossSectionProps xsp;
+
+    for (i = 1; i < 10; i++) {
+        depth         = (double) i;
+        xsp           = xs_hydraulic_properties (xs, depth);
+        critical_flow = xsp_get (xsp, XS_CRITICAL_FLOW);
+        xsp_free (xsp);
+
+        critical_depth = xs_critical_depth (xs, critical_flow, 1.25 * depth);
+        g_assert_true (test_is_close (depth, critical_depth, 0, 0.01));
+    }
+
+    coarray_free (ca);
+    xs_free (xs);
+}
+
+void
 add_xs_new_test ()
 {
     int    n        = 5;
@@ -609,4 +642,6 @@ add_crosssection_tests ()
     add_xs_double_triangle_test ();
     add_xs_coarray_test ();
     add_xs_roughness_test ();
+    g_test_add_func ("/panthera/xs/crosssection/xs_critical_depth",
+                     test_critical_depth);
 }
